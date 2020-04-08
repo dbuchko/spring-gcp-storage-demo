742ebed3b7a146e086cc4e3ff0f61cf05553b762
Daniel Buchko <dbuchko@pivotal.io>
2020-04-08 17:26:20 -0400

    Initial checkin

---
+++ src/main/java/com/tanzu/gcpstoragedemo/GcpstoragedemoApplication.java
@@ -0,0 +1,13 @@
+package com.tanzu.gcpstoragedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpstoragedemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcpstoragedemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tanzu/gcpstoragedemo/GcpstoragedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanzu.gcpstoragedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpstoragedemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
